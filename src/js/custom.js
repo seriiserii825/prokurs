1c11cdbecf4ddb802ed6f68c514036d3a133519a
serii <seriiburduja@gmail.com>
2017-06-03 13:08:40 +0300

    finish slider

--- src/js/custom.js
+++ src/js/custom.js
@@ -14,5 +14,46 @@ $(function () {
 		$("#js-header-tabs .wrapper .tab").removeClass("active").eq($(this).index()).addClass("active");
 		$("#js-header-tabs .tab_item").hide().eq($(this).index()).fadeIn();
 	}).eq(0).addClass("active");
+
+	/*stellar.js
+	===============================*/
+	$.stellar({
+		responsive: true
+	});
+
+	/*slick
+	===============================*/
+	$('#js-slider').slick({
+		dots: true,
+	  speed: 800,
+	  responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        arrows: false
+      }
+    }  
+  ]
+	});
+
+	$('#js-slider').hover(function(){
+		$('#js-slider .slick-prev, #js-slider .slick-next').removeClass('hover').addClass('hover');
+	},
+	function(){
+			$('#js-slider .slick-prev, #js-slider .slick-next').removeClass('hover');
+		}
+	);
+
+	/*magnific-popup
+	===============================*/
+	$('#js-slider').magnificPopup({
+	  delegate: 'a', // child items selector, by clicking on it popup will open
+	  type: 'image',
+	  // other options
+	  gallery: {
+	    // options for gallery
+	    enabled: true
+  	},
+	});
     
 }());
